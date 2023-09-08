9df31a78d07cc4b76d88088939e0d989e0d03792
Mariya Nikiforova <manu11shet@gmail.com>
2023-09-08 16:03:19 +0700

    add fontawesome

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,8 @@
 import "../node_modules/bootstrap/scss/bootstrap.scss";
+import "./fontawesome-6.4.2/scss/fontawesome.scss";
+import "./fontawesome-6.4.2/scss/brands.scss";
+import "./fontawesome-6.4.2/scss/solid.scss";
+import "./fontawesome-6.4.2/scss/regular.scss";
 import './css/app.css';
 
 const $ = require('jquery');
